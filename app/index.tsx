4dd94c656cdaf8e4f07c0b2b253d0d39916d1c64
Khoa Dang <dangkhoa080998@gmail.com>
2025-04-21 08:43:53 +0700

    re-structure

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+import { Component } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+export default class Home extends Component {
+    render() {
+        return (
+            <SafeAreaView>
+                <Text>index</Text>
+            </SafeAreaView>
+        )
+    }
+}
